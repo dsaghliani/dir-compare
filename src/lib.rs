43860adb0e69e40ebe7e4a8f53b54477838314b1
Demna Saghliani <saghliani.d@gmail.com>
2024-04-24 12:10:22 +0400

    Add example code to README & crate docs.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,15 @@ A crate for comparing a pair of files or directories—or their content. Constru
 Rust data structure; they both implement [`PartialEq`] & [`Eq`].
 
 This crate works recursively and compares both names and byte content.
+
+```
+use dir_compare::Content;
+
+let a = Content::of("fixtures/should-eq/dir-a").unwrap();
+let b = Content::of("fixtures/should-eq/dir-b").unwrap();
+
+assert_eq!(a, b);
+```
 */
 
 use core::fmt;
